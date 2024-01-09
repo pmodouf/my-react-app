76f6f4e912611debf5e197cf135c1f8db96a582d
pmodouf <pmodouf@gmail.com>
2024-01-09 13:42:27 +0100

    React POD ws

---
+++ src/products.js
@@ -0,0 +1,61 @@
+import product1 from "./assets/Laptop WS.webp";
+import product2 from "./assets/airpods WS.jpg";
+import product3 from "./assets/apple_ipad-pro WS.jpg";
+import product4 from "./assets/apple-iphone-11 WS.jpg";
+import product5 from "./assets/Hörlurar WS.webp";
+import product6 from "./assets/Lampa WS.jpg";
+import product7 from "./assets/PlayStation-5 WS.webp";
+import product8 from "./assets/TV WS.jpg";
+
+
+export const PRODUCTS = [
+
+    {
+        id: 1,
+        productName: "Laptop",
+        price: 1200.0,
+        productImage: product1, 
+    },
+    {
+        id: 2,
+        productName: "Airpods",
+        price: 100.0,
+        productImage: product2, 
+    },
+    {
+        id: 3,
+        productName: "Ipad-Pro",
+        price: 600.0,
+        productImage:product3, 
+    },
+    {
+        id: 4,
+        productName: "Iphone",
+        price: 800.0,
+        productImage: product4, 
+    },
+    {
+        id: 5,
+        productName: "Head Phones",
+        price: 300.0,
+        productImage: product5, 
+    },
+    {
+        id: 6,
+        productName: "Lamp",
+        price: 50.0,
+        productImage: product6, 
+    },
+    {
+        id: 7,
+        productName: "Playstation 5",
+        price: 800.0,
+        productImage: product7, 
+    },
+    {
+        id: 8,
+        productName: "TV",
+        price: 700.0,
+        productImage: product8, 
+    },
+];
